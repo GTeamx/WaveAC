cbf3de10cead77f28104c7ea98b8e935f4fa7721
XIII___ <cyjallet@gmail.com>
2023-06-10 21:58:08 +0200

    Renamed isLiquid to isWater, improved isWater and added isLava, isClimable, isCobweb and delays

--- src/main/java/com/xiii/wave/exempt/Exempt.java
+++ src/main/java/com/xiii/wave/exempt/Exempt.java
@@ -3,6 +3,10 @@ package com.xiii.wave.exempt;
 import com.xiii.wave.managers.profile.Profile;
 import com.xiii.wave.playerdata.data.impl.MovementData;
 import com.xiii.wave.utils.BetterStream;
+import com.xiii.wave.utils.MathUtils;
+import org.bukkit.Material;
+
+import java.util.List;
 
 public class Exempt {
 
@@ -12,24 +16,57 @@ public class Exempt {
         this.profile = profile;
     }
 
-    private boolean fly, liquid;
+    private boolean fly, water, lava, climable, cobweb;
+
+    private long lastWater, lastLava, lastClimable, lastCobweb;
 
     public void handleExempts(long timeStamp) {
 
         MovementData movementData = profile.getMovementData();
 
+        final List<Material> nearbyBlocks = movementData.getNearbyBlocks();
+
         //Fly
         this.fly = movementData.getLastFlyingAbility() < (20*5); //5s
 
         //Water Liquid
-        this.liquid = BetterStream.anyMatch(movementData.getNearbyBlocks(), mat -> mat.toString().contains("WATER"));
+        this.water = BetterStream.anyMatch(nearbyBlocks, mat -> mat == Material.WATER  || mat == Material.BUBBLE_COLUMN || mat == Material.LEGACY_STATIONARY_WATER || mat == Material.LEGACY_WATER);
+
+        //Lava Liquid
+        this.lava = BetterStream.anyMatch(nearbyBlocks, mat -> mat == Material.LAVA || mat == Material.LEGACY_LAVA || mat == Material.LEGACY_STATIONARY_LAVA);
+
+        //Climables
+        this.climable = BetterStream.anyMatch(nearbyBlocks, mat -> mat == Material.LADDER || mat == Material.VINE || mat == Material.TWISTING_VINES || mat == Material.TWISTING_VINES_PLANT || mat == Material.WEEPING_VINES || mat == Material.WEEPING_VINES_PLANT || mat == Material.LEGACY_VINE || mat == Material.LEGACY_LADDER);
+
+        //Cobweb
+        this.cobweb = BetterStream.anyMatch(nearbyBlocks, mat -> mat == Material.COBWEB);
+
+        if (this.water) this.lastWater = System.currentTimeMillis();
+
+        if (this.lava) this.lastLava = System.currentTimeMillis();
+
+        if (this.climable) this.lastClimable = System.currentTimeMillis();
+
+        if (this.cobweb) this.lastCobweb = System.currentTimeMillis();
     }
 
     public boolean isFly() {
         return fly;
     }
 
-    public boolean isLiquid() {
-        return liquid;
+    public boolean isWater(final long delay) {
+        return MathUtils.elapsed(lastWater) <= delay;
+    }
+
+    public boolean isLava(final long delay) {
+        return MathUtils.elapsed(lastLava) <= delay;
+    }
+
+    public boolean isClimable(final long delay) {
+        return MathUtils.elapsed(lastClimable) <= delay;
+    }
+
+    public boolean isCobweb(final long delay) {
+        return MathUtils.elapsed(lastCobweb) <= delay;
     }
 }
