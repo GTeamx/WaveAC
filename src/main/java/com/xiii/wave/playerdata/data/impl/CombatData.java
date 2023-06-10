f4518bc116884bacb48fa829a38eab4714902ab9
XIII___ <cyjallet@gmail.com>
2023-06-10 21:57:17 +0200

    Debugging action data

--- src/main/java/com/xiii/wave/playerdata/data/impl/CombatData.java
+++ src/main/java/com/xiii/wave/playerdata/data/impl/CombatData.java
@@ -1,14 +1,33 @@
 package com.xiii.wave.playerdata.data.impl;
 
+import com.xiii.wave.managers.profile.Profile;
 import com.xiii.wave.playerdata.data.Data;
+import com.xiii.wave.playerdata.processors.impl.SetbackProcessor;
 import com.xiii.wave.processors.Packet;
+import com.xiii.wave.utils.TaskUtils;
+import com.xiii.wave.utils.custom.Equipment;
+import org.bukkit.Bukkit;
 
 public class CombatData implements Data {
 
+    private final Profile profile;
+
+    private final Equipment equipment;
+
+    public CombatData(Profile profile) {
+        this.profile = profile;
+
+        this.equipment = new Equipment();
+    }
+
     @Override
     public void process(Packet packet) {
-        /*
-        Handle the packet
-         */
+
+        switch (packet.getType()) {
+
+            case ENTITY_ACTION:
+
+                TaskUtils.task(() -> Bukkit.broadcastMessage(packet.getEntityActionWrapper() + ""));
+        }
     }
 }
