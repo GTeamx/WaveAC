14f2571b7fc42a4230bd7e0b5d0d91b971c51ba6
XIII___ <cyjallet@gmail.com>
2023-06-11 23:02:37 +0200

    Made vars final

--- src/main/java/com/xiii/wave/checks/types/Check.java
+++ src/main/java/com/xiii/wave/checks/types/Check.java
@@ -3,16 +3,19 @@ package com.xiii.wave.checks.types;
 import com.xiii.wave.checks.enums.CheckType;
 import com.xiii.wave.managers.profile.Profile;
 import com.xiii.wave.processors.packet.client.ClientPlayPacket;
+import com.xiii.wave.processors.packet.server.ServerPlayPacket;
 
 public abstract class Check extends AbstractCheck {
 
-    public Check(Profile profile, CheckType check, String type, String description) {
+    public Check(final Profile profile, final CheckType check, final String type, final String description) {
         super(profile, check, type, description);
     }
 
-    public Check(Profile profile, CheckType check, String description) {
+    public Check(final Profile profile, final CheckType check, final String description) {
         super(profile, check, "", description);
     }
 
-    public abstract void handle(ClientPlayPacket clientPlayPacket);
+    public abstract void handle(final ClientPlayPacket clientPlayPacket);
+
+    public abstract void handle(final ServerPlayPacket serverPlayPacket);
 }
