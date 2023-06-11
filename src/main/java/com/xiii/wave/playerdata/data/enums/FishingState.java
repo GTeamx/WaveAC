7f11ccc9679f08375f5fd9bfbb6220a926b1fbaf
XIII___ <cyjallet@gmail.com>
2023-06-11 23:05:59 +0200

    Added fishing data

---
+++ src/main/java/com/xiii/wave/playerdata/data/enums/FishingState.java
@@ -0,0 +1,6 @@
+package com.xiii.wave.playerdata.data.enums;
+
+public enum FishingState {
+
+    BITE, BITE_FAIL, CAUGHT, UNKNOWN
+}
---
+++ src/main/java/com/xiii/wave/playerdata/data/impl/FishingData.java
@@ -0,0 +1,76 @@
+package com.xiii.wave.playerdata.data.impl;
+
+import com.github.retrooper.packetevents.protocol.entity.data.EntityData;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata;
+import com.xiii.wave.managers.profile.Profile;
+import com.xiii.wave.playerdata.data.Data;
+import com.xiii.wave.playerdata.data.enums.FishingState;
+import com.xiii.wave.processors.packet.client.ClientPlayPacket;
+import com.xiii.wave.processors.packet.server.ServerPlayPacket;
+
+public class FishingData implements Data {
+
+    private final Profile profile;
+
+    private FishingState fishingState = FishingState.UNKNOWN;
+
+    private long lastBite = 10000L, lastBiteFail = 10000L, lastCaught = 10000L;
+
+    public FishingData(final Profile profile) {
+        this.profile = profile;
+    }
+
+    @Override
+    public void process(final ClientPlayPacket clientPlayPacket) {
+    }
+
+    @Override
+    public void process(final ServerPlayPacket serverPlayPacket) {
+
+        if (serverPlayPacket.getType() == PacketType.Play.Server.ENTITY_METADATA) {
+
+            final WrapperPlayServerEntityMetadata wrapperPlayServerEntityMetadata = serverPlayPacket.getEntityMetadataWrapper();
+
+            for (final EntityData entityData : wrapperPlayServerEntityMetadata.getEntityMetadata()) {
+
+                final Object value = entityData.getValue();
+                final Object index = entityData.getIndex();
+
+                //Bite successful
+                if (value.equals(true) && index.equals(9)) {
+                    this.fishingState = FishingState.BITE;
+                    this.lastBite = serverPlayPacket.getTimeStamp();
+                }
+
+                //Bite fail
+                if (value.equals(false) && index.equals(9)) {
+                    this.fishingState = FishingState.BITE_FAIL;
+                    this.lastBiteFail = serverPlayPacket.getTimeStamp();
+                }
+
+                //Caught
+                if (index.equals(16)) {
+                    this.fishingState = FishingState.CAUGHT;
+                    this.lastCaught = serverPlayPacket.getTimeStamp();
+                }
+            }
+        }
+    }
+
+    public FishingState getFishingState() {
+        return fishingState;
+    }
+
+    public long getLastBite() {
+        return lastBite;
+    }
+
+    public long getLastBiteFail() {
+        return lastBiteFail;
+    }
+
+    public long getLastCaught() {
+        return lastCaught;
+    }
+}
