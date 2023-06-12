20a151be21046ce9dff440532f2432aedf800d6f
XIII___ <cyjallet@gmail.com>
2023-06-12 23:45:07 +0200

    Added falses and bypasses. Cleaned up code

--- src/main/java/com/xiii/wave/checks/impl/speed/SpeedA.java
+++ src/main/java/com/xiii/wave/checks/impl/speed/SpeedA.java
@@ -9,31 +9,34 @@ import com.xiii.wave.processors.PredictionEngine;
 import com.xiii.wave.processors.packet.client.ClientPlayPacket;
 import com.xiii.wave.processors.packet.server.ServerPlayPacket;
 
+/*
+FALSES:
+BYPASSES:
+ */
+
 @Experimental
 public class SpeedA extends Check {
-
-    public SpeedA(Profile profile) {
+    public SpeedA(final Profile profile) {
         super(profile, CheckType.SPEED, "A", "Checks for irregular movement");
     }
 
     @Override
-    public void handle(ClientPlayPacket clientPlayPacket) {
+    public void handle(final ClientPlayPacket clientPlayPacket) {
+
         if (!clientPlayPacket.isMovement()) return;
 
-        final boolean exempt = profile.isExempt().isFly() || profile.isExempt().isWater(20l) || profile.isExempt().isWall();
+        final boolean exempt = profile.isExempt().isFly() || profile.isExempt().isWater(50L) || profile.isExempt().isWall(5);
 
         final MovementData data = profile.getMovementData();
+
         final double[] predictions = PredictionEngine.getHorizontalPrediction(profile);
 
-        if (data.getDeltaXZ() >= predictions[0] && predictions[1] > 7.0E-5 && data.getDeltaX() != 0 && predictions[0] != 0 && !exempt) {
-            if (increaseBuffer() > 4 || predictions[1] > 1) {
-                fail("xz=" + Math.abs(data.getDeltaXZ() - predictions[0]) + " s=" + predictions[1]);
-            }
+        if (!exempt && data.getDeltaXZ() >= predictions[0] && predictions[1] > 7.0E-5 && data.getDeltaX() != 0 && predictions[0] != 0) {
+
+            if (increaseBuffer() > 4 || predictions[1] > 1) fail("xz=" + Math.abs(data.getDeltaXZ() - predictions[0]) + " s=" + predictions[1]);
         } else decreaseBufferBy(1);
     }
 
     @Override
-    public void handle(ServerPlayPacket serverPlayPacket) {
-
-    }
+    public void handle(final ServerPlayPacket serverPlayPacket) {}
 }
