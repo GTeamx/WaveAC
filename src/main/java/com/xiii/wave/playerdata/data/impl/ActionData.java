fc0f530f90a901f9a6cb540f219b954268dea0dd
XIII___ <cyjallet@gmail.com>
2023-06-15 21:59:24 +0200

    Attempt to add block placement

--- src/main/java/com/xiii/wave/playerdata/data/impl/ActionData.java
+++ src/main/java/com/xiii/wave/playerdata/data/impl/ActionData.java
@@ -2,14 +2,18 @@ package com.xiii.wave.playerdata.data.impl;
 
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientEntityAction;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerBlockPlacement;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerBlockAction;
 import com.xiii.wave.Wave;
 import com.xiii.wave.managers.profile.Profile;
 import com.xiii.wave.playerdata.data.Data;
 import com.xiii.wave.processors.packet.client.ClientPlayPacket;
 import com.xiii.wave.processors.packet.server.ServerPlayPacket;
 import com.xiii.wave.utils.MiscUtils;
+import com.xiii.wave.utils.TaskUtils;
 import com.xiii.wave.utils.custom.PlacedBlock;
 import com.xiii.wave.utils.custom.desync.Desync;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -50,26 +54,30 @@ public class ActionData implements Data {
 
     @Override
     public void process(final ClientPlayPacket clientPlayPacket) {
+
         if (clientPlayPacket.is(PacketType.Play.Client.ENTITY_ACTION)) {
+
             final WrapperPlayClientEntityAction action = clientPlayPacket.getEntityActionWrapper();
-            if(action.getAction() == WrapperPlayClientEntityAction.Action.START_SPRINTING) {
-                this.sprinting = true;
-            }
-            if(action.getAction() == WrapperPlayClientEntityAction.Action.STOP_SPRINTING) {
-                this.sprinting = false;
-            }
-            if(action.getAction() == WrapperPlayClientEntityAction.Action.START_SNEAKING) {
-                this.sneaking = true;
-            }
-            if(action.getAction() == WrapperPlayClientEntityAction.Action.STOP_SNEAKING) {
-                this.sneaking = false;
-            }
+
+            if(action.getAction() == WrapperPlayClientEntityAction.Action.START_SPRINTING) this.sprinting = true;
+
+            if(action.getAction() == WrapperPlayClientEntityAction.Action.STOP_SPRINTING) this.sprinting = false;
+
+            if(action.getAction() == WrapperPlayClientEntityAction.Action.START_SNEAKING) this.sneaking = true;
+
+            if(action.getAction() == WrapperPlayClientEntityAction.Action.STOP_SNEAKING) this.sneaking = false;
         }
     }
 
     @Override
     public void process(final ServerPlayPacket serverPlayPacket) {
 
+        if (serverPlayPacket.is(PacketType.Play.Server.BLOCK_ACTION)) {
+
+            final WrapperPlayServerBlockAction blockActionWrapper = serverPlayPacket.getBlockActionWrapper();
+
+            TaskUtils.task(() -> Bukkit.broadcastMessage("adata=" + blockActionWrapper.getActionData()));
+        }
     }
 
     public int getLastRidingTicks() {
