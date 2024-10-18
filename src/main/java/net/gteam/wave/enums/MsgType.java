433817e5818040303724fa9f7d5509b5bce357cb
XIII-MC <xiii-mc@github.com>
2024-10-18 11:55:44 +0200

    Changed wave prefix Changed no permissions message Changed colors

--- src/main/java/net/gteam/wave/enums/MsgType.java
+++ src/main/java/net/gteam/wave/enums/MsgType.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum MsgType {
-    PREFIX("§b§lWave §8»"),
-    NO_PERMISSION(PREFIX.getMessage() + "No perms"),
+    PREFIX("§b§l\uD83C\uDF0A "), // 🌊
+    NO_PERMISSION(PREFIX.getMessage() + "Missing permissions."), // TODO: Replace with spigot's "unknown command" message
     CONSOLE_COMMANDS(PREFIX.getMessage() + "Not for console"),
-    ALERT_MESSAGE(PREFIX.getMessage() + "&f%player% &7failed &b%check% &7x%vl%"),
+    ALERT_MESSAGE(PREFIX.getMessage() + "&b%player% &7flagged &b%check% &7x%vl%"),
     ALERT_HOVER(stringFromList(Arrays.asList(
             "&7Description:&r",
             "%description%",
