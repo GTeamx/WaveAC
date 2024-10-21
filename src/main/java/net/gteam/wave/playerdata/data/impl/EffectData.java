b27e2b4d51201564b2230ec754507b73f1463ebe
XIII-MC <xiii-mc@github.com>
2024-10-21 10:43:30 +0200

    made code readable

--- src/main/java/net/gteam/wave/playerdata/data/impl/EffectData.java
+++ src/main/java/net/gteam/wave/playerdata/data/impl/EffectData.java
@@ -1,6 +1,5 @@
 package net.gteam.wave.playerdata.data.impl;
 
-import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityEffect;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerRemoveEntityEffect;
@@ -10,7 +9,6 @@ import net.gteam.wave.processors.ClientPlayPacket;
 import net.gteam.wave.processors.ServerPlayPacket;
 import net.gteam.wave.utils.custom.CustomEffect;
 import net.gteam.wave.utils.custom.EffectType;
-import org.bukkit.Bukkit;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -19,27 +17,33 @@ public class EffectData implements Data {
     private final Profile profile;
     private final ConcurrentHashMap<EffectType, CustomEffect> effects = new ConcurrentHashMap<>();
 
-    public EffectData(Profile profile) {
+    public EffectData(final Profile profile) {
         this.profile = profile;
     }
 
     @Override
-    public void process(final ClientPlayPacket packet) {
-
-    }
+    public void process(final ClientPlayPacket ignored) {}
 
     @Override
     public void process(final ServerPlayPacket packet) {
+
         switch (packet.getType()) {
+
             case ENTITY_EFFECT:
+
                 final WrapperPlayServerEntityEffect entityEffect = packet.getEntityEffectWrapper();
                 final CustomEffect effect = new CustomEffect(entityEffect.getPotionType().getId(profile.getVersion()), entityEffect.getEffectAmplifier(), entityEffect.getEffectDurationTicks());
+
                 effects.put(EffectType.fromID(entityEffect.getPotionType().getId(ClientVersion.V_1_12)), effect);
+
                 break;
 
             case REMOVE_ENTITY_EFFECT:
+
                 final WrapperPlayServerRemoveEntityEffect removeEntityEffect = packet.getRemoveEntityEffectWrapper();
+
                 effects.remove(EffectType.fromID(removeEntityEffect.getPotionType().getId(ClientVersion.V_1_12)));
+
                 break;
         }
 
