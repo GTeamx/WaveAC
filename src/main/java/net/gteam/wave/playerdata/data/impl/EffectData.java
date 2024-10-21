172370ca1a5c312ebdfa3e352c04561b229db055
XIII-MC <xiii-mc@github.com>
2024-10-21 12:29:19 +0200

    changed from EffectType to PotionEffectType

--- src/main/java/net/gteam/wave/playerdata/data/impl/EffectData.java
+++ src/main/java/net/gteam/wave/playerdata/data/impl/EffectData.java
@@ -1,21 +1,21 @@
 package net.gteam.wave.playerdata.data.impl;
 
-import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityEffect;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerRemoveEntityEffect;
+import io.github.retrooper.packetevents.util.SpigotConversionUtil;
 import net.gteam.wave.managers.profile.Profile;
 import net.gteam.wave.playerdata.data.Data;
 import net.gteam.wave.processors.ClientPlayPacket;
 import net.gteam.wave.processors.ServerPlayPacket;
 import net.gteam.wave.utils.custom.CustomEffect;
-import net.gteam.wave.utils.custom.EffectType;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 public class EffectData implements Data {
 
     private final Profile profile;
-    private final ConcurrentHashMap<EffectType, CustomEffect> effects = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<PotionEffectType, CustomEffect> effects = new ConcurrentHashMap<>();
 
     public EffectData(final Profile profile) {
         this.profile = profile;
@@ -34,7 +34,7 @@ public class EffectData implements Data {
                 final WrapperPlayServerEntityEffect entityEffect = packet.getEntityEffectWrapper();
                 final CustomEffect effect = new CustomEffect(entityEffect.getPotionType().getId(profile.getVersion()), entityEffect.getEffectAmplifier(), entityEffect.getEffectDurationTicks());
 
-                effects.put(EffectType.fromID(entityEffect.getPotionType().getId(ClientVersion.V_1_12)), effect);
+                this.effects.put(SpigotConversionUtil.toBukkitPotionEffectType(entityEffect.getPotionType()), effect);
 
                 break;
 
@@ -42,14 +42,14 @@ public class EffectData implements Data {
 
                 final WrapperPlayServerRemoveEntityEffect removeEntityEffect = packet.getRemoveEntityEffectWrapper();
 
-                effects.remove(EffectType.fromID(removeEntityEffect.getPotionType().getId(ClientVersion.V_1_12)));
+                this.effects.remove(SpigotConversionUtil.toBukkitPotionEffectType(removeEntityEffect.getPotionType()));
 
                 break;
         }
 
     }
 
-    public ConcurrentHashMap<EffectType, CustomEffect> getEffects() {
+    public ConcurrentHashMap<PotionEffectType, CustomEffect> getEffects() {
         return effects;
     }
 }
